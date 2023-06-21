e956f5bad34641334faddb72d15eef4f3d93a186
Aditya <60221087+adityaprao@users.noreply.github.com>
2023-06-21 16:53:23 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (53.39%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    public:
+    bool isLongPressedName(string name, string typed) {
+        int m = name.length(), n = typed.length();
+        int i = 0, j = 0;
+        
+        while(i < m && j < n)
+        {
+            char c1 = name[i], c2 = typed[j];
+            if(c1 != c2) return false; 
+            
+			// count of consecutive c1/c2
+            int count1 = 0; 
+            while(i < m && name[i] == c1){
+                count1++, i++;
+            }
+            
+            int count2 = 0; 
+            while(j < n && typed[j] == c2){
+                count2++, j++;
+            }
+            
+            if(count2 < count1) return false;
+        }
+        
+		// they both reach the end
+        return i == m && j == n;
+    }
+};
