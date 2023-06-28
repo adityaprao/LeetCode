db8758a063041fc2030fb27bd0107ba90b2836d1
Aditya <60221087+adityaprao@users.noreply.github.com>
2023-06-29 00:51:11 +0530

    Time: 43 ms (85.11%), Space: 31.1 MB (88.50%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,28 +1,10 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int, int>> pq;
-        for (int i = 0; i < arr.size(); i++)
-        {
-            pq.push({abs(arr[i] - x), i});
-            if (pq.size() > k) pq.pop();
-        }
-        vector<int> ans;
-        // while (pq.size())
-        // {
-        //     cout << pq.top().first << arr[pq.top().second] << endl;
-        //     int idx = pq.top().second;
-        //     // cout << arr[idx] << " ";
-        //     pq.pop();
-        // }
-        while (!pq.empty())
-        {
-            //cout << pq.top().first << " " << pq.top().second << endl;
-            ans.push_back(arr[pq.top().second]);
-            pq.pop();
-        }
-        sort(ans.begin(), ans.end());
-        return ans;
-        
+        int L = 0, R = size(arr)-1;
+        while (R - L >= k) 
+            if (x - arr[L] <= arr[R] - x) R--;
+            else L++;
+        return vector<int>(begin(arr) + L, begin(arr) + R + 1);
     }
 };
