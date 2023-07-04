092d15b0ce1a5a448cc5367fafd134864b14c912
Aditya <60221087+adityaprao@users.noreply.github.com>
2023-07-05 00:02:07 +0530

    Time: 7 ms (82.86%), Space: 9.5 MB (41.15%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int a=0;
+        int b=0;
+        for(int c:nums)
+        {
+            int ta=(~a&b&c)|(a&~b&~c);
+            b=(~a&~b&c)|(~a&b&~c);
+            a=ta;
+        }
+        return a|b;
+        
+    }
+};
