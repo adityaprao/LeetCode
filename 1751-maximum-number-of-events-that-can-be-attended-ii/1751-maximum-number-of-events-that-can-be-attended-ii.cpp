74db41bcf09a7ff3ff2be4c10e86efe6b82e528b
Aditya <60221087+adityaprao@users.noreply.github.com>
2023-07-15 10:19:22 +0530

    Time: 266 ms (98.89%), Space: 65.6 MB (84.20%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxValue(vector<vector<int>>& events, int k) {
+        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+        
+        int n = events.size();
+        dp = vector<vector<int>>(k + 1, vector<int>(n, -1));
+        
+        return dfs(0, k, events);
+    }    
+    
+private:
+    vector<vector<int>> dp;
+    
+    int dfs(int curIndex, int count, const vector<vector<int>>& events) {
+        if (count == 0 || curIndex == events.size()) {
+            return 0;
+        }
+        if (dp[count][curIndex] != -1) {
+            return dp[count][curIndex];
+        }
+        int nextIndex = bisectRight(events, events[curIndex][1]);
+        dp[count][curIndex] = max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
+        return dp[count][curIndex];
+    }
+    
+    static int bisectRight(const vector<vector<int>>& events, int target) {
+        int left = 0, right = events.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (events[mid][0] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }   
+};
+
+
+
+
+
+
+
